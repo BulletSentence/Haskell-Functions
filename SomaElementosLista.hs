4e63a9cc08d970540ac3ccb7adab4ce9fb32cf3d
Leonardo Almeida <leonardo.er404@gmail.com>
2019-06-29 17:18:32 -0300

    Soma Elementos de uma lista e retorna o total

--- SomaElementosLista.hs
+++ SomaElementosLista.hs
@@ -1,5 +1,11 @@
+import Prelude
+
 --Soma os elementos de uma lista --
 
-somaLista :: [Int]->Int
-somaLista [] = 0
-somalista (x:xs) = x + somaLista xs
+somaLista :: [Int] -> Int
+somaLista [ ] = 0
+somaLista (x:xs) = x + somaLista xs
+
+somaLista1 :: [Int]->Int
+somaLista1 [ ] = 0
+somaLista1 (x:xs) = sum [x] + somaLista1 xs 
