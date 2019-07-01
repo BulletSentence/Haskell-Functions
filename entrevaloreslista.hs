c78c84067c8123bcb6b613f87fe195a93264cb74
Leonardo Almeida <leonardo.er404@gmail.com>
2019-07-01 07:38:48 -0300

    retorna os valores entre dois indices da lista

---
+++ entrevaloreslista.hs
@@ -0,0 +1,8 @@
+-- retorna os valores entre dois indices da lista --
+
+slice :: [a] -> Int -> Int -> [a]
+slice [] _ _  = []
+slice (x:xs) i k
+ | i > 1      = slice xs (i - 1) (k - 1)
+ | k < 1      = []
+ | otherwise  = x:slice xs (i - 1) (k - 1)
